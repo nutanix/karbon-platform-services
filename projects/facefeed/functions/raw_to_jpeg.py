f3904f166a29b0ac19954efea97a7615b1a4a9c5
Andy Daniel <andy.daniel@nutanix.com>
2019-01-28 21:30:35 -0500

    Update raw_to_jpeg.py
    Added max_bin_len param to msgpack.unpackb to handle larger frames from RTSP sources.

--- projects/facefeed/functions/raw_to_jpeg.py
+++ projects/facefeed/functions/raw_to_jpeg.py
@@ -10,7 +10,7 @@ import base64
 import json
 
 def main(ctx,msg):
-    unpacked_dict = msgpack.unpackb(msg, raw=False)
+    unpacked_dict = msgpack.unpackb(msg, raw=False, max_bin_len=3145728)
     image =  numpy.fromstring(unpacked_dict["Data"], dtype=unpacked_dict["DataType"])
     image = image.reshape((unpacked_dict["Height"],unpacked_dict["Width"],unpacked_dict["Channels"]))
     cvImage = cv2.cvtColor(image,cv2.COLOR_RGB2BGR)
