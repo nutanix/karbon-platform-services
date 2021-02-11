38014338e374366c7901986eac1fd8c401b2695f
skekre98 <sharvil.kekre@nutanix.com>
2021-02-11 15:49:49 -0800

    removed static passwords

--- projects/woodkraft/Part2/loader/run.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-echo "Starting loader"
-
-locust -f /loadgen.py --headless -u $USER -r 1 -H $WP_HOST
--- projects/woodkraft/Part2/recommendation-service/debezium-setup.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-curl -kv -i -X POST http://debezium-connect:8083/connectors \
- -H "Accept:application/json" \
- -H "Content-Type:application/json" \
- -d '{ "name": "wordpress-connector", "config": { "connector.class": "io.debezium.connector.mysql.MySqlConnector", "tasks.max": "1", "database.hostname": "mysql", "database.port": "3306", "database.user": "root", "database.password": "R00tMysql", "database.server.id": "184055", "database.server.name": "wordpress_db", "database.whitelist": "wordpress_db", "database.history.kafka.bootstrap.servers": "sherlock-kafka-svc:9092", "database.history.kafka.topic": "dbhistory.wordpress", "database.allowPublicKeyRetrieval": "true" } }'
-
