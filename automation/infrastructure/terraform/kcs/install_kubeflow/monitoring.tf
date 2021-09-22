1157cdd2c49fa8ff8c5985e61fc2cff34c93cf35
Aleksei Kaplin <alexey.kaplin@nutanix.com>
2021-09-22 16:38:36 +0200

    Update installation

--- automation/infrastructure/terraform/kcs/install_kubeflow/monitoring.tf
+++ automation/infrastructure/terraform/kcs/install_kubeflow/monitoring.tf
@@ -27,6 +27,11 @@ resource "null_resource" "prepare_prometheus" {
   provisioner "local-exec" {
     command = "kubectl --kubeconfig=${self.triggers.kubeconfig_filename} apply -f monitoring.ns.yaml"
   }
+
+  depends_on = [
+    null_resource.now,
+    module.karbon_kube_config
+  ]
 }
 
 resource "null_resource" "install_prometheus" {
@@ -42,5 +47,10 @@ resource "null_resource" "install_prometheus" {
     command = "helm install --kubeconfig=${self.triggers.kubeconfig_filename} -f monitoring.env.yaml --version ${self.triggers.kube_prometheus_stack_version} kubeflow-monitoring prometheus-community/kube-prometheus-stack"
   }
 
-  depends_on = [null_resource.now, null_resource.helm_setup, null_resource.prepare_prometheus]
+  depends_on = [
+    null_resource.now,
+    module.karbon_kube_config,
+    null_resource.helm_setup,
+    null_resource.prepare_prometheus
+  ]
 }
